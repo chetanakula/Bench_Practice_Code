4630706003dc9c9efb5e573c09e0c958d0be76cc
chetanakula <115332203+chetanakula@users.noreply.github.com>
2023-05-10 19:03:52 +0530

    C# programs

---
+++ 10-05-2023/37.Remove_Spaces_From_String.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_csharp
+{
+    class Remove_Spaces_From_String
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            Console.WriteLine("After removing spaces");
+            string str1 = str.Replace(" ", "");
+            Console.WriteLine(str1);
+        }
+    }
+}
---
+++ 10-05-2023/39.Captilise_First_Last_Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_csharp
+{
+    class Captilise_First_Last_Words
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            string str1 = str.Substring(0, 1).ToUpper() + str.Substring(1, str.Length - 2) + str.Substring(str.Length - 1).ToUpper();
+            Console.WriteLine(str1);
+        }
+    }
+}
